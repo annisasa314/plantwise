6131b4c42b044a7881378b54fca538955b4bdefd
Siti Nur Anisah <anisahsn22d@student.unhas.ac.id>
2024-11-21 23:38:43 +0800

    hide firebase configuration

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKD23Mq2sYpu_UqBw3MUBC6L7xPOW1XHE",
-  authDomain: "plantwise-8eadf.firebaseapp.com",
-  projectId: "plantwise-8eadf",
-  storageBucket: "plantwise-8eadf.firebasestorage.app",
-  messagingSenderId: "625351268886",
-  appId: "1:625351268886:web:8c54e16e0e87cba5bf7337",
-  measurementId: "G-WR0P0EQE1W" 
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Inisialisasi aplikasi Firebase
