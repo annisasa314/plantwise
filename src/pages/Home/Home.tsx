873c48f5eee286a6e340d3cf3196a124e7dac503
suciartiara <artiaras22d@student.unhas.ac.id>
2024-11-21 21:23:27 +0800

    feat: add login and signup page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../../components/ExploreContainer";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
+        <Navbar />
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
