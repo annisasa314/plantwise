b440ee02cf9a7684a790466f812510f1a782786d
suciartiara <artiaras22d@student.unhas.ac.id>
2024-12-06 18:27:38 +0800

    chore: tambah dependensi tailwind, react query

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,jsx,tsx}",
+    "./node_modules/@ionic/react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
