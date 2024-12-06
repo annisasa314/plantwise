6784bfc5f503d1f9e8b3dedf1d1d215444f83301
suciartiara <artiaras22d@student.unhas.ac.id>
2024-12-06 18:54:02 +0800

    feat: tambah footer sementara

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { IonFooter, IonGrid, IonRow, IonCol } from "@ionic/react";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <IonFooter className="footer">
+      <IonGrid className="footer-container">
+        {/* Brand Section */}
+        <IonRow className="footer-row">
+          <IonCol sizeMd="4" sizeSm="12" className="footer-brand">
+            <div className="logo">
+              <img
+                src="/logo2.png"
+                alt="Plantwise Logo"
+                className="logo-img-footer"
+              />
+            </div>
+            <p className="footer-description">
+              Duis feugiat mauris sit amet dolor dapibus, vitae malesuada nibh
+              porta. Sed dui leo, aliquet in tortor condimentum, pellentesque
+              convallis magna.
+            </p>
+            <p className="copyright">©2024 plantwise. Semua Hak Dilindungi.</p>
+          </IonCol>
+
+          {/* Links Section */}
+          <IonCol sizeMd="4" sizeSm="6" className="footer-links">
+            <ul>
+              <li>
+                <a href="#">Beranda</a>
+              </li>
+              <li>
+                <a href="#">Daftar / Masuk</a>
+              </li>
+              <li>
+                <a href="#">Tentang Kami</a>
+              </li>
+              <li>
+                <a href="#">Syarat & Ketentuan</a>
+              </li>
+              <li>
+                <a href="#">Kebijakan Privasi</a>
+              </li>
+            </ul>
+          </IonCol>
+
+          {/* Social Media Section */}
+          <IonCol sizeMd="4" sizeSm="6" className="footer-socials">
+            <a href="#">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-tiktok"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-youtube"></i>
+            </a>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonFooter>
+  );
+};
+
+export default Footer;
