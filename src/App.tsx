25d81835d991c5adfd3b21a61564aa4e94bba801
suciartiara <artiaras22d@student.unhas.ac.id>
2024-12-06 18:30:12 +0800

    chore: tambah react query di app.tsx dan home, profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,61 @@
+import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import Home from "./pages/Home/Home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Import your pages
 import Login from "./pages/Login/Login";
-import SignUp from "./pages/SignUp/SignUp";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/SignUp/SignUp";
 
-/* Core CSS required for Ionic components to work properly */
+// Ionic CSS
 import "@ionic/react/css/core.css";
-
-/* Basic CSS for apps built with Ionic */
 import "@ionic/react/css/normalize.css";
 import "@ionic/react/css/structure.css";
 import "@ionic/react/css/typography.css";
 
-/* Optional CSS utils that can be commented out */
+// Optional CSS
 import "@ionic/react/css/padding.css";
 import "@ionic/react/css/float-elements.css";
 import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-// import "@ionic/react/css/palettes/dark.system.css";
-
-/* Theme variables */
-import "./theme/variables.css";
+import Profile from "./pages/Profile/Profile";
 
 setupIonicReact();
 
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      // cacheTime: 30 * 60 * 1000, // 30 minutes
+    },
+  },
+});
+
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/signup">
-          <SignUp />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+  <QueryClientProvider client={queryClient}>
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/home" component={Home} />
+          {/* <Route exact path="/forum" component={Forum} /> */}
+          {/* <Route exact path="/calculator" component={Calculator} /> */}
+          <Route exact path="/profile" component={Profile} />
+
+          {/* Default redirect */}
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  </QueryClientProvider>
 );
 
 export default App;
