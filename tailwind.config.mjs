2b59410f070c08c9f92db05adf0549adb1d45273
suciartiara <artiaras22d@student.unhas.ac.id>
2024-12-10 22:20:36 +0800

    change tailwind.config.js to mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{html,ts,jsx,tsx}",
+//     "./node_modules/@ionic/react/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["Poppins", "Arial", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+export default {
+  content: [
+    "./src/**/*.{html,ts,jsx,tsx}",
+    "./node_modules/@ionic/react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
