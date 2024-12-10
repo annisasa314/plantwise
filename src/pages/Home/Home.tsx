8cbca561166355c2a5dd26dc862253d61ac1055e
suciartiara <artiaras22d@student.unhas.ac.id>
2024-12-10 21:06:32 +0800

    fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,10 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonFooter,
-  IonText,
-  IonImg,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonButton,
-  IonCardTitle,
-  IonCard,
-  IonCardHeader,
-  IonCardContent,
-} from "@ionic/react";
-import Navbar from "../../components/Navbar/Navbar";
+import { IonContent, IonGrid } from "@ionic/react";
 import "./Home.css";
 import Footer from "../../components/Footer/Footer";
 import HeroSection from "../../components/Hero/Hero";
 import { MainLayout } from "../../layouts/MainLayout";
 import About from "../../components/About/About";
 import VisionMission from "../../components/Misi/Misi";
-import CardFitur from "../../components/Fitur/CardFitur";
-import TutorialCard from "../../components/Fitur/CardFitur";
 import TutorialGrid from "../../components/Fitur/Fitur";
 import { ChatbotProvider } from "../../components/Bot/ChatbotProvider";
 import Chatbot from "../../components/Bot/Chatbot";
@@ -44,11 +24,17 @@ const Home: React.FC = () => {
                 <VisionMission />
               </section>
 
-              <section style={{ marginTop: "80px", marginBottom: "10px" }}>
+              <section
+                style={{
+                  marginTop: "80px",
+                  marginBottom: "10px",
+                }}
+              >
                 <TutorialGrid />
               </section>
             </section>
           </IonGrid>
+
           {/* Chatbot components */}
           <ChatbotProvider>
             <Chatbot />
